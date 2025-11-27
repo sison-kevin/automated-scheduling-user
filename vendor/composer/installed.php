a2acf8c26df79ab387cc120ffd69fe765993a433
monicabacay <monicabacay3@gmail.com>
2025-11-27 23:39:24 +0800

    First commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'pet/auto-sched',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '1c8e49d1bcb917abee0eee843a23dd4c145ea455',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'pet/auto-sched' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '1c8e49d1bcb917abee0eee843a23dd4c145ea455',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
