06154515e7595eb456ac3d9561d68a95462590d8
monicabacay <monicabacay3@gmail.com>
2025-11-27 23:26:24 +0800

    First commit

--- Userpanel-web_project/LavaLust-dev-v4/app/config/database.php
+++ Userpanel-web_project/LavaLust-dev-v4/app/config/database.php
@@ -59,11 +59,11 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 $database['main'] = array(
     'driver'	=> 'mysql',
-    'hostname'	=> 'localhost',
+    'hostname'	=> 'sql12.freesqldatabase.com',
     'port'		=> '3306',
-    'username'	=> 'root',
-    'password'	=> '',
-    'database'	=> 'vet_system',
+    'username'	=> 'sql12809653',
+    'password'	=> 'TCU6zusERl',
+    'database'	=> 'sql12809653',
     'charset'	=> 'utf8mb4',
     'dbprefix'	=> '',
     // Optional for SQLite
